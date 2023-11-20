7d92a371904e03ca2517263bc9c1f8343d207b11
Elis Solence <97757342+ElisSolence@users.noreply.github.com>
2023-11-20 09:34:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os;path="/home"
 while True:
  command = input(f"ethernet_man@ubuntu-linux:~{path}$ ")
-  if command.startswith("cd "):
-   command=command.split(" ",1)[1].split(";",1)[0]
-   if command=="..":
-    if"/"in path:path=path.rsplit("/",1)[0]
-    else:path=""
-   elif os.system(f"cd {path}; cd {command}")==0:
-    if path=="~"or command.startswith("/"):path=command
-    else:path+="/"+command;command=""
-    if path.endswith("/"):path=path[0:-1]
-    if command!="":os.system(f"cd {path}; {command}")
+ if command.startswith("cd "):
+  command=command.split(" ",1)[1].split(";",1)[0]
+  if command=="..":
+   if"/"in path:path=path.rsplit("/",1)[0]
+   else:path=""
+  elif os.system(f"cd {path}; cd {command}")==0:
+   if path=="~"or command.startswith("/"):path=command
+   else:path+="/"+command;command=""
+ if path.endswith("/"):path=path[0:-1]
+ if command!="":os.system(f"cd {path}; {command}")
